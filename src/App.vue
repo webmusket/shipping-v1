9839fe3ab27bdc1da754b026963cbd5c3046ec74
Md Asaduzzaman <sultanasad8@gmail.com>
2020-03-04 00:03:23 +0600

    added the login module

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,6 +16,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +25,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@/assets/css/tailwind.css'
+
 Vue.config.productionTip = false
+
+
+  import axios from 'axios'; 
+  Vue.use(axios)
 
 new Vue({
   render: h => h(App),
