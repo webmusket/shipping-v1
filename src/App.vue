d9449bbda1cd43495090019cee90620a3606216b
Md Asaduzzaman <sultanasad8@gmail.com>
2020-03-04 07:11:57 +0600

    break

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Login msg="Welcome to Your Vue.js App"/> -->
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // Login
   }
 }
 </script>
